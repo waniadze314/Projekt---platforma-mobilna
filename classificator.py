1c9edafed2695faa8e35bb9596fec15e516bfa4a
Krzysztof <kzajac1997@gmail.com>
2019-05-03 19:03:39 +0200

    Add classificator net architecture

--- classificator.py
+++ classificator.py
@@ -1,36 +1,73 @@
 import sys
 sys.path.insert(0,'..')
 
-import numpy as np
-import pandas as pd
 import mxnet as mx
 from mxnet import nd
 from mxnet import autograd
-from mxnet import init
 from mxnet import gluon
-from mxnet import image
-from mxnet import contrib
-from mxnet.gluon import nn
+from mxnet import init
+
 from mxnet.gluon import data as gluon_data
 from mxnet.gluon import loss as gluon_loss
-from mxnet.gluon import utils as gluon_utils
+from mxnet.gluon import nn 
 
-import d2l
+import os
+import pandas as pd
+import shutil
+import time
+from tqdm import tqdm
 
-class detector(nn.Block):
-    def __init__(self):
-        super(detector,self).__init__(**kwargs)
-        
-        self.net = nn.Sequential()
-        self.net.add(nn.Dense(256,activation='relu'))
-        
-        self.net.initialize(init.Normal(sigma=0.1))
-    
+class Recognizer(nn.HybridBlock):
+    def __init__(self,**kwargs):
+        super(Recognizer,self).__init__(**kwargs)
+        self.ctx = mx.cpu_shared() 
+
+        self.net = nn.HybridSequential()
+        with self.net.name_scope():
+            self.net.add(nn.Conv2D(3,kernel_size=3,padding=1,strides=1,activation='relu'))
+            self.net.add(nn.BatchNorm())
+            self.net.add(nn.Conv2D(3,kernel_size=3,padding=1,strides=1,activation='relu'))
+            self.net.add(nn.BatchNorm())
+            self.net.add(nn.MaxPool2D())
+            self.net.add(nn.Dropout(0.2))
+            self.net.add(nn.Conv2D(3,kernel_size=3,padding=1,strides=1,activation='relu'))
+            self.net.add(nn.BatchNorm())
+            self.net.add(nn.Conv2D(3,kernel_size=3,padding=1,strides=1,activation='relu'))
+            self.net.add(nn.BatchNorm())
+            self.net.add(nn.MaxPool2D())
+            self.net.add(nn.Conv2D(3,kernel_size=3,padding=1,strides=1,activation='relu'))
+            self.net.add(nn.BatchNorm())
+            self.net.add(nn.Conv2D(3,kernel_size=3,padding=1,strides=1,activation='relu'))
+            self.net.add(nn.BatchNorm())
+            self.net.add(nn.MaxPool2D())
+            self.net.add(nn.Flatten())
+            self.net.add(nn.Dense(3,activation='relu'))
+            
+            self.net.initialize(init.Xavier())
+
+        self.net.hybridize()
+
+        self.loss_values = [] # array for training visualisation
+
+    # Overwrite forward pass
     def forward(self, x):
-        for block in self._children.values():
-            x = block(x)
+        return self.net(x)
 
-        return x
+    # train model
+    def train(self,num_epochs,train_iter,loss_function,trainer,batch_size):
+        total_loss = 0.0
+        print("Training: ")
+        for epoch in tqdm(range(1,num_epochs+1)):
+            for data,label in train_iter:
+                # autograd.record() switches dropout layers on
+                with autograd.record():
+                    y_hat = self.net(data)
+                    loss_value = loss_function(y_hat,label).sum()
 
-    def save_params(self):
-        self.net.save_parameters('classificator.params')
+                total_loss += loss_value.asscalar()
+                loss_value.backward()
+                trainer.step(batch_size)
+            
+            print('epoch %d, loss %.2f' % (epoch, total_loss))
+            self.loss_values.append(total_loss)
+            total_loss = 0.0
