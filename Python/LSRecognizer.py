943c8e791ace9887594845df4af0de77454765f3
Krzysztof <kzajac1997@gmail.com>
2019-05-22 00:49:20 +0200

    Update Canny params

--- Python/LSRecognizer.py
+++ Python/LSRecognizer.py
@@ -5,18 +5,18 @@ import numpy as np
 def ls_match(image,template):
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     template = cv2.Canny(template, 10, 25)
-    (h,w) = template.shape[:2]
+    (h,w) = template.shape[:2] # get width and height
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # set to grey 
     
+    # resize image in loop to scan it fully
     for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-        #resize the image and store the ratio
-        resized_img = imutils.resize(image, width = int(image.shape[1] * scale))
-        ratio = image.shape[1] / float(resized_img.shape[1])
-        if resized_img.shape[0] < h or resized_img.shape[1] < w:
+        resized_image = imutils.resize(image, width = int(image.shape[1] * scale))
+        
+        if resized_image.shape[0] < h or resized_image.shape[1] < w:
             break
 
-        canny = cv2.Canny(resized_img, 10, 25)
+        canny = cv2.Canny(resized_image, 10, 25) # detect egdes
         match = cv2.matchTemplate(canny, template, cv2.TM_CCOEFF)
     
     return match
