4b68891d28ffda956c6be6f945cbd174b03b2d14
Krzysztof <kzajac1997@gmail.com>
2019-05-22 13:52:50 +0200

    Update Main

--- Python/Main.py
+++ Python/Main.py
@@ -14,37 +14,48 @@ import glob
 from image import *
 from LSRecognizer import *
 
-delay = 1
-
 if __name__ == "__main__":
     camera = cv2.VideoCapture(1)
     template = cv2.imread("D:\LSDataset\datatemplate.png")
-    counter = 0
-    threshold = 0.5
-    detected_index = 1
-    start_time = time.time()
+    # set constants
+    delay = 1 # delay time in seconds
+    counter = 0 # counter collecting detected images
+    threshold = 0.5 # initial threshold for gradient recognizer
+    detected_index = 1 # utility index for saving detected images to directory
+    # greater value gives more certanity for detecting image 
+    # but lower probability forlow quality images
+    detection_constat = 3 
+    start_time = time.time() # start time 
+    # Main loop
     while True:
-        _,image = camera.read()
-        match = ls_match(image,template)
-        if get_threshold(match) > threshold:
+        _,image = camera.read() # take picture
+        match = ls_match(image,template) # match with gradient recognizer
+        # if image is matched update counter
+        if get_threshold(match) > threshold: 
             counter += 1
-            threshold += 0.1
-            delay += 1
-
+            threshold += 0.1 # increase threshold to avid false detections
+            delay += 1 # icrease delay to allow robot for better positiong
+        # if initial detection was false
         else:
-            counter = 0
+            counter = 0 # reset counter threshold and delay
             threshold = 0.5
             delay = 1
 
-        print(get_threshold(match))
-        if counter >= 3:
-            cv2.imshow('',image)
+        # print(get_threshold(match))
+        
+        # if image was detected
+        if counter >= detection constat:
+            # display image
+            # cv2.imshow('',image)
+            # cv2.waitKey()
             cv2.imwrite('detected'+str(detected_index)+'.png')
-            print(time.time() - start_time) 
+            # save image to file
+            with open("times.txt","a") as times_file:
+                times_file.write(str(time.time() - start_time))
+            # reset threshold counter and delay
             detected_index += 1
-            cv2.waitKey()
             threshold = 0.5
             counter = 0 
             delay = 1
-        
+        # wait 
         time.sleep(delay)
